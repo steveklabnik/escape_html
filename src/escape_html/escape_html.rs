99002fe4bdb437d1ffd9eb26804854f54bd3825a
Steve Klabnik <steve@steveklabnik.com>
2013-09-13 16:02:07 -0700

    Adding in tests.
    They're just marked to fail right now.

--- src/escape_html/escape_html.rs
+++ src/escape_html/escape_html.rs
@@ -22,7 +22,45 @@ fn mark_safe(s: QuestionableString<UnsafeString>) -> QuestionableString<SafeStri
 */
 
 #[test]
+#[should_fail]
 fn test_escaping() {
-    let input = mark_unsafe(~"<script>");
-    assert_eq!(escape_html(input), ~"<script>");
+    let input = mark_unsafe(~"<script>\"'&</script>");
+    assert_eq!(escape_html(input), ~"&lt;script&gt;&#34;&#39;&amp;&lt;/script&gt;");
 }
+
+
+#[test]
+#[should_fail]
+fn test_escaping_lt() {
+    let input = mark_unsafe(~"<");
+    assert_eq!(escape_html(input), ~"&lt;");
+}
+
+#[test]
+#[should_fail]
+fn test_escaping_gt() {
+    let input = mark_unsafe(~">");
+    assert_eq!(escape_html(input), ~"&gt;");
+}
+
+#[test]
+#[should_fail]
+fn test_escaping_amp() {
+    let input = mark_unsafe(~"&");
+    assert_eq!(escape_html(input), ~"&amp;");
+}
+
+#[test]
+#[should_fail]
+fn test_escaping_quote() {
+    let input = mark_unsafe(~"'");
+    assert_eq!(escape_html(input), ~"&#39;");
+}
+
+#[test]
+#[should_fail]
+fn test_escaping_double_quote() {
+    let input = mark_unsafe(~"\"");
+    assert_eq!(escape_html(input), ~"&#34;");
+}
+
